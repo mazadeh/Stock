5373cb8ea12ca8601f382d79a12621d4f46aee6a
mazadeh94 <m.azadeh94@gmail.com>
2016-05-05 07:39:35 +0430

    Tomcat is installed on the server (mohammad's pc) now we could go on air.
    visit:
    	http://localhost:8080/Stock/
    	http://localhost:8080/Stock/hello
    	*http://localhost:8080/Stock/web/stock.html
    * (don't forget to run json-server, stock.json is needed to load users table)

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+if [ -z "$CATALINA_HOME" ]; then
+    echo "The environment variable CATALINA_HOME must be set to the root of the Tomcat installation directory"
+    exit 1
+fi  
+
+APP_CONTEXT=Stock
+
+sudo cp target/$APP_CONTEXT.war $CATALINA_HOME/webapps/
