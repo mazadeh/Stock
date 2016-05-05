1a34d20edda807b0702968a9ea7a82f4b66e0018
arghavanmh <arghavan.mh.ut@gmail.com>
2016-05-05 12:08:08 +0430

    Update arghyApp.js

--- src/main/webapp/js/arghyApp.js
+++ src/main/webapp/js/arghyApp.js
@@ -1,1 +1,33 @@
+(function(){
 
+	var app = angular.module('stock', []);
+	var Users;
+
+	app.controller('StockController', ['$http', function($http){
+		var usersCtrl = this;
+		this.users = [];
+		this.symbols = [];
+		this.selected = null;
+	
+		$http.get('http://localhost:3000/users').success(function(pollsData) {
+		    usersCtrl.users = pollsData;
+		});
+		$http.get('http://localhost:3000/symbols').success(function(symbolsData) {
+		    usersCtrl.symbols = symbolsData;
+		});
+		
+	   this.symbolClicked = function(id) {
+            if (this.selected != id) {
+                this.selected = id;           
+            }
+        };
+
+        this.isSelected = function (id) {
+            return id === this.selected;
+        };
+		
+		
+		
+	}]);
+	
+})();
