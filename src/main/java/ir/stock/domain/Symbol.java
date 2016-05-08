2517eb9ec7af1d368e64fe652ad8876eec8537e8
arghavanmh <arghavan.mh.ut@gmail.com>
2016-05-08 18:36:14 +0430

    Commercial-carousel Added Commercial-Carousel to Stock.html Added SellBuy Lists To Symbol.java

--- src/main/java/ir/stock/domain/Symbol.java
+++ src/main/java/ir/stock/domain/Symbol.java
@@ -1,14 +1,22 @@
 package ir.stock.domain;
 
+import java.util.List;
+
 public class Symbol
 {
 	private int id;
 	private String name;
 	
+	private List<SellBuyRequest> sellList;
+	private List<SellBuyRequest> buyList;
+	
 	public Symbol( int id, String name)
 	{
 		this.id = id;
 		this.name = name;
+		
+		sellList = new List<SellBuyRequest>();
+		buyList = new List<SellBuyRequest>();
 	}
 	
 	public int getId()
