a21eab729d9821d3d03640559d7f01fdb62e5fe1
mazadeh94 <m.azadeh94@gmail.com>
2016-05-09 14:04:49 +0430

    HSQLDB startup changed Now we check whether database exists or not.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$CATALINA_HOME" ]; then
-    echo "The environment variable CATALINA_HOME must be set to the root of the Tomcat installation directory"
-    exit 1
-fi  
-
-APP_CONTEXT=stock
-
-sudo cp target/$APP_CONTEXT.war $CATALINA_HOME/webapps/
--- sh/hsqldb-startup.sh
+++ sh/hsqldb-startup.sh
@@ -1,4 +1,14 @@
-cd ../src/database/data-log
+cd ../src/database
+
+if [ -d data-log ]
+then
+	echo Database exists
+else
+	echo Database created
+	mkdir -p data-log
+fi
+cd data-log
+
 echo 'New Run' >> hsqldb.log
 date >> hsqldb.log
 echo 'Server is running in the background...'
